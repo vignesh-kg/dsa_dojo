103bcb8f2bd920047090be28f29c07e3e32ef4ff
vignesh-kg <vigneshkg515@gmail.com>
2024-10-28 12:56:42 +0200

    permutations.java

---
+++ AgodaInterviewPrep/Permutations.java
@@ -0,0 +1,37 @@
+package AgodaInterviewPrep;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permutations {
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    for(List<Integer> permutation : s.permute(new int[]{1,2,3})){
+      System.out.print(permutation + " ");
+    }
+    System.out.print("\n");
+  }
+}
+
+class Solution {
+  public List<List<Integer>> permute(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+    backtrack(result, new ArrayList<>(), nums, new boolean[nums.length]);
+    return result;
+  }
+
+  private void backtrack(List<List<Integer>> result, List<Integer> subset, int[] nums, boolean[] used){
+    if(subset.size() == nums.length){
+      result.add(new ArrayList<>(subset));
+      return;
+    }
+    for(int i = 0; i < nums.length; ++i){
+      if(used[i]) continue;
+      subset.add(nums[i]);
+      used[i] = true;
+      backtrack(result, subset, nums, used);
+      subset.remove(subset.size()-1);
+      used[i] = false;
+    }
+  }
+}
